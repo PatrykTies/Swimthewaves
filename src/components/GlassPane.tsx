5fa1f87dbdb8f2008607a38b1208808b01b78151
Patryk <patryk.ties@lovehomeswap.com>
2023-02-12 11:15:17 +0000

    add Glass pane

---
+++ src/components/GlassPane.tsx
@@ -0,0 +1,13 @@
+import clsx from 'clsx';
+import React from 'react';
+
+type GlassPaneProps = {
+    children: React.ReactNode;
+    className?: string;
+}
+
+const GlassPane = ({children, className} : GlassPaneProps) => {
+    return <div className={clsx('glass rounded-2xl border-solid border-2 border-gray-200', className)}>{children}</div>
+}
+
+export default GlassPane;
